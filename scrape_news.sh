e9c4e133f98bc1ac83ae536a8339d1aee9f4fa20
snoam16 <snoam16@gmail.com>
2020-12-10 12:17:50 +0200

    the third commit

--- scrape_news.sh
+++ scrape_news.sh
@@ -3,15 +3,17 @@
 #copy the main page to file named 3082
 wget https://www.ynetnews.com/category/3082
 
-#copy the links from file 3082 to links.txt without repetitions
-grep -o "https://www.ynetnews.com/article/[a-zA-Z0-9]\{9\}" 3082 |\
- uniq > links.txt
-wc -l links.txt | cut -c1,2 >> results.csv
+#copy the links from file 3082 to links_first.txt without repetitions
+grep -o "https://www.ynetnews.com/article/[a-zA-Z0-9]\{9\}\"" 3082 |\
+ uniq > links_first.txt
+#delete the last char in each line(")
+sed 's/.$//' links_first.txt > links.txt
+#counting number of links
+wc -l links.txt | cut -c1,2 > results.csv
 #run on each link page and count bibi and gantz appearences
 for link in `cat links.txt`;
 do
    wget -q $link -O article.txt 
-   
    count_bibi=`grep -c  "Netanyahu" article.txt`
    count_gantz=`grep -c  "Gantz" article.txt`
 #checking if there were any appearnces of bibi and/or gantz in this article
